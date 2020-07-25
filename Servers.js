3a180a6e18b35059d59cc8906c63797b2ae01169
Zen20031 <65483617+Zen20031@users.noreply.github.com>
2020-07-25 22:55:51 +0300

    Update Servers.js

--- Servers.js
+++ Servers.js
@@ -15864,7 +15864,7 @@ msg.reply("test")
         .setFooter(`Server name: ${msg.guild.name}`)
           .setColor('#ffff00')
           reeeeeeeeeeeeet= await client.channels.cache.get(x).send(r);
-          return msg.channel.send("<@&730638478454423623>  **E B I C** tier 5 spawned, go get it <:pog:699999258547322950>")   
+          return msg.channel.send("<@&735457819565883413>  **E B I C** tier 5 spawned, go get it <:pog:699999258547322950>")   
         } 
       if(msg.embeds[0].title.includes('Tier: 6'))
       { tr=`https://discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}`;
@@ -15903,7 +15903,7 @@ msg.reply("test")
         .setFooter(`Server name: ${msg.guild.name}`)
           .setColor('#ea2222')
           reeeeeeeeeeeeet= await client.channels.cache.get(x).send(r);
-          return msg.channel.send("<@&730638478454423623>  **E B I C** tier 6 spawned, go get it <:pog:699999258547322950>") 
+          return msg.channel.send("<@&735457819565883413>  **E B I C** tier 6 spawned, go get it <:pog:699999258547322950>") 
         } 
       //console.log("WE HAVE A PROBLEM!!!");// if there are no embeds, this code won't run.
         });
