1f62b4eb074abdf91a28ff7f5dddc749eb0b3ef1
Zen20031 <65483617+Zen20031@users.noreply.github.com>
2020-08-04 09:28:35 +0300

    Update Servers.js

--- Servers.js
+++ Servers.js
@@ -8694,7 +8694,7 @@ client.on('message',async msg => {
     }
 //logs!!!
     if(msg.embeds[0].title.includes('Tier: 1'))
-  {msg.channel.send("<@&740087343217639477> -T4 TEST <@&740087340411650118>-T5 TEST <@&737570525563322408>-T5 TEST");
+  {
       let xp=14;
       let myinterval;
       let ok=1;
