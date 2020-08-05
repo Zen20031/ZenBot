2774141b440212373e1a0d67dfcb5ae99d5b15f5
Zen20031 <65483617+Zen20031@users.noreply.github.com>
2020-08-05 22:15:24 +0300

    Update Servers.js

--- Servers.js
+++ Servers.js
@@ -5122,7 +5122,7 @@ let ret;
 let nammess;
 client.on('message',async msg => {
 //DESPAWN SERVER 1 DADDY'S CAVE
-  if((msg.channel.id==="738561941337014303") && (msg.author.id==="673362753489993749")){
+  if((msg.channel.id==="740647658993090560") && (msg.author.id==="673362753489993749")){
     if((msg.content.startsWith('Looks like nobody')) && (msg.author.id==="673362753489993749"))
     { let tr=0;
       if(tiier===1)
