cde32ef26ce7930ee7797dd7760e3710b5db6359
Zen20031 <65483617+Zen20031@users.noreply.github.com>
2020-08-13 09:18:32 +0300

    Update Servers.js

--- Servers.js
+++ Servers.js
@@ -19874,7 +19874,7 @@ msg.reply("test")
            .setFooter(`Server name: ${msg.guild.name}`)
              .setColor('#cc00ff')
            reeeeeeeeeeeeeeeeeeeeeet= await client.channels.cache.get(x).send(r);
-          return  msg.channel.send("<@&719935847839039516>  **E B I C** tier 4 spawned, go get it <:pog:699999258547322950>") 
+          return  //msg.channel.send("<@&719935847839039516>  **E B I C** tier 4 spawned, go get it <:pog:699999258547322950>") 
            }
          if(msg.embeds[0].title.includes('Tier: 5'))
           {tr=`https://discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}`;
@@ -19913,7 +19913,7 @@ msg.reply("test")
            .setFooter(`Server name: ${msg.guild.name}`)
              .setColor('#ffff00')
              reeeeeeeeeeeeeeeeeeeeeet= await client.channels.cache.get(x).send(r);
-             return msg.channel.send("<@&719935847839039516>  **E B I C** tier 5 spawned, go get it <:pog:699999258547322950>")   
+             return //msg.channel.send("<@&719935847839039516>  **E B I C** tier 5 spawned, go get it <:pog:699999258547322950>")   
            } 
          if(msg.embeds[0].title.includes('Tier: 6'))
          { tr=`https://discord.com/channels/${msg.guild.id}/${msg.channel.id}/${msg.id}`;
@@ -19952,7 +19952,7 @@ msg.reply("test")
            .setFooter(`Server name: ${msg.guild.name}`)
              .setColor('#ea2222')
              reeeeeeeeeeeeeeeeeeeeeet= await client.channels.cache.get(x).send(r);
-             return msg.channel.send("<@&719935847839039516>  **E B I C** tier 6 spawned, go get it <:pog:699999258547322950>") 
+             return //msg.channel.send("<@&719935847839039516>  **E B I C** tier 6 spawned, go get it <:pog:699999258547322950>") 
            } 
          //console.log("WE HAVE A PROBLEM!!!");// if there are no embeds, this code won't run.
            });
