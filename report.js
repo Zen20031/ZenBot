f26c026d97c2a9f032ef424b021eb8d51381b333
Zen20031 <65483617+Zen20031@users.noreply.github.com>
2020-08-21 22:39:28 +0300

    Delete report.js

--- report.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-const reportSchema =mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    username: String,
-    userID: String
-})
-
-module.exports=mongoose.model("Report", reportSchema)
